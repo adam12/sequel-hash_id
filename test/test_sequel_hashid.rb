007c7a6f81db76e2cf3a1e6a9d18c1885225b6b4
Adam Daniels <adam@mediadrive.ca>
2017-02-10 15:56:26 -0500

    Add tests

--- test/test_sequel_hashid.rb
+++ test/test_sequel_hashid.rb
@@ -1,5 +1,46 @@
 require "minitest/autorun"
+require "sequel"
 
 class TestSequelHashId < Minitest::Test
+  def setup
+    @db = Sequel.mock(fetch: { id: 1 })
+    @model = Class.new(Sequel::Model(@db[:test]))
+  end
 
+  def test_salt_required
+    assert_raises ArgumentError do
+      @model.plugin :hash_id
+    end
+  end
+
+  def test_custom_length
+    @model.plugin :hash_id, salt: "the-salt", length: 10
+
+    instance = @model.new
+    def instance.id
+      15
+    end
+
+    assert_equal 10, instance.hashid.length
+  end
+
+  def test_with_hashid
+    @model.plugin :hash_id, salt: "the-salt"
+    instance = @model.create
+
+    assert_equal instance, @model.with_hashid(instance.hashid)
+  end
+
+  def test_invalid_hashid
+    @model.plugin :hash_id, salt: "the-salt"
+
+    assert_nil @model.with_hashid("invalid")
+  end
+
+  def test_dataset_method
+    @model.plugin :hash_id, salt: "the-salt"
+    hashid = @model.hasher.encode(1)
+
+    refute_nil @model.dataset.with_hashid(hashid)
+  end
 end
